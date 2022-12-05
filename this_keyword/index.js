e7f341347f5a1f4be3881a4b4d601fda986a8f45
Budescu Eftimie <budescueftimie@gmail.com>
2022-12-05 03:32:14 +0100

    add bind method example

--- this_keyword/index.js
+++ this_keyword/index.js
@@ -10,7 +10,12 @@ console.log(car.getBrand()); // Honda
 let brand = car.getBrand; // in this line the getBrand method
                           // is stored inside brand variable 
 
-console.log(brand());    // undefined
+console.log(brand());    // undefined because this refers now to the window obj
+
+// to fix it we have to bind the getBrand method
+
+brand = car.getBrand.bind(car)
+console.log(brand());  // now the code works and we get Honda.
 
 
 
